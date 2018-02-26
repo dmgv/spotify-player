927ebb76e518c2b1ea23a125a47ac718cd922d53
Diego M. G. Vieira <diego.menezzes@gmail.com>
2018-02-26 08:37:23 -0300

    Add components: AlbumTracks & AlbumInfo

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,7 +1,8 @@
 import SpotifyWrapper from "spotify-api-wrapper";
 
 const spotify = new SpotifyWrapper({
-  token: "Your token here"
+  token:
+    "BQAAXZhByuV-5ROtPL38lLmpmKrH_PQhdEGX0FTOcRcH7D1ASVCKzuPYtgjNRhYs7XyJR8CFSHm3k1vAI-L-QkBYaFF4pUukdJMTwkAhbOotXa80dfUc9-8zUHtltiVFYeJLYi0IAzaRG6DWRw"
 });
 
 export default spotify;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-console.log('Spotify Player!');
+import spotify from "./Spotify";
+import renderAlbums from "./AlbumList";
+import renderAlbumInfo from "./AlbumInfo";
+import renderAlbumTracks from "./AlbumTracks";
+
+const albums = spotify.search.albums("Incubus");
+const albumList = document.getElementById("album-list");
+
+const album = spotify.album.getAlbum("2i6nd4FV6y7K9fln6eelmR");
+const albumInfo = document.getElementById("album-info");
+const albumTracks = document.getElementById("album-tracks");
+
+albums.then(data => renderAlbums(data.albums.items, albumList));
+
+album
+  .then(data => renderAlbumInfo(data, albumInfo))
+  .then(data => renderAlbumTracks(data.tracks.items, albumTracks));
